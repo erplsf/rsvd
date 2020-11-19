5d6199b32f8d082e14d4cb8ef52e14d1c4da300f
Andriy Mykhaylyk <erp.lsf@gmail.com>
2020-11-19 21:39:00 +0100

    add log move window into struct

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 extern crate sdl2;
 extern crate ash;
 
-use crate::ash::version::EntryV1_0;
-
+use ash::version::{InstanceV1_0, EntryV1_0};
 use sdl2::video::{Window};
 use ash::{vk, Entry, Instance};
 use std::ffi::{CString};
@@ -14,28 +13,46 @@ struct VulkanGod {
 }
 
 impl VulkanGod {
-    fn new(window: Window) -> Self {
+    fn new(width: u32, height: u32) -> Self {
         unsafe {
             let entry: Entry = Entry::new().unwrap();
-            let instance = Self::create_instance(&entry);
+            let window = Self::create_window(width, height);
+            let instance = Self::create_instance(&window, &entry);
 
             VulkanGod{
-                window,
                 entry,
+                window,
                 instance
             }
         }
     }
 
-    unsafe fn create_instance(entry: &Entry) -> Instance {
+    fn create_window(width: u32, height: u32) -> Window {
+        let sdl_context = sdl2::init().unwrap();
+        let video_subsystem = sdl_context.video().unwrap();
+        
+        video_subsystem.window("rsvd", width, height)
+            .position_centered()
+            .build()
+            .unwrap()
+    }
+
+    unsafe fn create_instance(window: &Window, entry: &Entry) -> Instance {
         let name = CString::new("rsvd").unwrap();
         let appinfo = vk::ApplicationInfo::builder()
             .application_name(&name)
             .application_version(vk::make_version(0, 1, 0))
             .api_version(vk::make_version(1, 2, 0));
 
+        let required_extensions = window.vulkan_instance_extensions().unwrap();
+
+        let required_extensions_raw = required_extensions.iter()
+            .map(|ext| CString::new(*ext).unwrap().as_ptr())
+            .collect::<Vec<_>>();
+
         let create_info = vk::InstanceCreateInfo::builder()
-            .application_info(&appinfo);
+            .application_info(&appinfo)
+            .enabled_extension_names(&required_extensions_raw);
         
         let instance = entry
             .create_instance(&create_info, None)
@@ -45,14 +62,14 @@ impl VulkanGod {
     }
 }
 
-pub fn main() {    
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
-    
-    let window = video_subsystem.window("rsvd", 800, 600)
-        .position_centered()
-        .build()
-        .unwrap();
+impl Drop for VulkanGod {
+    fn drop(&mut self) {
+        unsafe {
+            self.instance.destroy_instance(None);
+        }
+    }
+}
 
-    let vg = VulkanGod::new(window);
+pub fn main() {    
+    let _vg = VulkanGod::new(800, 600);
 }
