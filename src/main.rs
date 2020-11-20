bb4360c2c8ba415a361b2ad4765a6bb503f46cea
Andriy Mykhaylyk <erp.lsf@gmail.com>
2020-11-20 08:58:55 +0100

    fix the stuff with sdl2 extensions

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,10 @@ extern crate ash;
 use ash::version::{InstanceV1_0, EntryV1_0};
 use sdl2::video::{Window};
 use ash::{vk, Entry, Instance};
-use std::ffi::{CString};
+use std::ffi::{CString, CStr};
+use std::os::raw::c_char;
+
+use ash::extensions::khr::{Surface, XlibSurface};
 
 struct VulkanGod {
     window: Window,
@@ -14,16 +17,14 @@ struct VulkanGod {
 
 impl VulkanGod {
     fn new(width: u32, height: u32) -> Self {
-        unsafe {
-            let entry: Entry = Entry::new().unwrap();
-            let window = Self::create_window(width, height);
-            let instance = Self::create_instance(&window, &entry);
+        let entry: Entry = Entry::new().unwrap();
+        let window = Self::create_window(width, height);
+        let instance = Self::create_instance(&window, &entry);
 
-            VulkanGod{
-                entry,
-                window,
-                instance
-            }
+        VulkanGod{
+            entry,
+            window,
+            instance
         }
     }
 
@@ -32,31 +33,50 @@ impl VulkanGod {
         let video_subsystem = sdl_context.video().unwrap();
         
         video_subsystem.window("rsvd", width, height)
+            .vulkan()
             .position_centered()
             .build()
             .unwrap()
     }
 
-    unsafe fn create_instance(window: &Window, entry: &Entry) -> Instance {
+    fn create_instance(window: &Window, entry: &Entry) -> Instance {
         let name = CString::new("rsvd").unwrap();
         let appinfo = vk::ApplicationInfo::builder()
             .application_name(&name)
             .application_version(vk::make_version(0, 1, 0))
-            .api_version(vk::make_version(1, 2, 0));
+            .api_version(vk::make_version(1, 0, 0));
 
+        // get required extensions from sdl2
         let required_extensions = window.vulkan_instance_extensions().unwrap();
 
-        let required_extensions_raw = required_extensions.iter()
-            .map(|ext| CString::new(*ext).unwrap().as_ptr())
-            .collect::<Vec<_>>();
+        dbg!(&required_extensions);
+
+        // convert them to raw pointers
+        let required_extensions_raw: Vec<_> = required_extensions.iter()
+            .map(|&ext| CString::new(ext).unwrap().into_raw())
+            .collect();
+
+        // cast them for the next call
+        let required_extensions_raw_const: Vec<_> = required_extensions_raw.iter()
+            .map(|&raw| raw as *const c_char)
+            .collect();
 
         let create_info = vk::InstanceCreateInfo::builder()
             .application_info(&appinfo)
-            .enabled_extension_names(&required_extensions_raw);
+            .enabled_extension_names(&required_extensions_raw_const);
+
+        let instance: Instance;
         
-        let instance = entry
+        unsafe {
+            instance = entry
             .create_instance(&create_info, None)
-            .expect("Instance creation error");
+                .expect("Instance creation error");
+
+            // recreate them from raw pointers so rust can deallocate them
+            for raw in required_extensions_raw {
+                CString::from_raw(raw);
+            }
+        }
 
         return instance
     }
