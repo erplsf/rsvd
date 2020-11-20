6560dfa82a2163d0656d6f115237e98d41b2244f
Andriy Mykhaylyk <erp.lsf@gmail.com>
2020-11-20 14:38:37 +0100

    use builder to better mimick vulkan-tutorial.com

--- src/main.rs
+++ src/main.rs
@@ -5,58 +5,99 @@ use std::borrow::Cow;
 use std::ffi::CStr;
 use ash::version::{InstanceV1_0, EntryV1_0};
 use sdl2::video::{Window};
-use ash::{vk, Entry, Instance};
+use ash::{vk, Entry, Instance, Device};
+use ash::vk::PhysicalDevice;
 use std::ffi::{CString};
 use std::os::raw::c_char;
 use ash::extensions::{ext::DebugUtils};
+
+const APP_NAME: &str = "rsvd";
 
 struct VulkanGod {
     window: Window,
     entry: Entry,
     instance: Instance,
-    debug_info: vk::DebugUtilsMessengerCreateInfoEXT,
+    // physical_device: PhysicalDevice,
     debug_utils_loader: DebugUtils,
     debug_call_back: vk::DebugUtilsMessengerEXT
 }
 
-impl VulkanGod {
-    fn new(width: u32, height: u32) -> Self {
-        let entry: Entry = Entry::new().unwrap();
-        let window = Self::create_window(width, height);
-        let debug_info = Self::create_debug_info();
-        let instance = Self::create_instance(&window, &entry, debug_info);
-        let (debug_utils_loader, debug_call_back) = Self::create_debug_utils_loader(&entry, &instance, &debug_info);
+struct VulkanGodBuilder {
+    width: u32,
+    height: u32,
+    window: Option<Window>,
+    entry: Option<Entry>,
+    instance: Option<Instance>,
+    // physical_device: Option<PhysicalDevice>,
+    debug_info: Option<vk::DebugUtilsMessengerCreateInfoEXT>,
+    debug_utils_loader: Option<DebugUtils>,
+    debug_call_back: Option<vk::DebugUtilsMessengerEXT>
+}
 
-        VulkanGod{
-            entry,
-            window,
-            instance,
-            debug_info,
-            debug_utils_loader,
-            debug_call_back
+impl VulkanGodBuilder {
+    fn new(width: u32, height: u32) -> Self {
+        Self{
+            width,
+            height,
+            window: None,
+            entry: None,
+            instance: None,
+            // physical_device: None,
+            debug_info: None,
+            debug_utils_loader: None,
+            debug_call_back: None
         }
     }
 
-    fn create_window(width: u32, height: u32) -> Window {
+    fn build(mut self) -> VulkanGod {
+        self.init_vulkan();
+        
+        VulkanGod{
+            entry: self.entry.unwrap(),
+            window: self.window.unwrap(),
+            instance: self.instance.unwrap(),
+            // physical_device,
+            debug_utils_loader: self.debug_utils_loader.unwrap(),
+            debug_call_back: self.debug_call_back.unwrap()
+        }
+    }
+
+    fn init_vulkan(&mut self) -> &mut Self {
+        self.create_instance();
+        self.setup_debug_messenger();
+        self
+    }
+
+    fn create_instance(&mut self) -> &mut Self {
+        self.entry = Some(Entry::new().unwrap());
+
         let sdl_context = sdl2::init().unwrap();
         let video_subsystem = sdl_context.video().unwrap();
         
-        video_subsystem.window("rsvd", width, height)
-            .vulkan()
-            .position_centered()
-            .build()
-            .unwrap()
-    }
+        self.window = Some(video_subsystem.window(APP_NAME, self.width, self.height)
+                           .vulkan()
+                           .position_centered()
+                           .build()
+                           .unwrap());
 
-    fn create_instance(window: &Window, entry: &Entry, mut debug_info: vk::DebugUtilsMessengerCreateInfoEXT) -> Instance {
-        let name = CString::new("rsvd").unwrap();
+        self.debug_info = Some(vk::DebugUtilsMessengerCreateInfoEXT::builder()
+                               .message_severity(
+                                   vk::DebugUtilsMessageSeverityFlagsEXT::ERROR
+                                       | vk::DebugUtilsMessageSeverityFlagsEXT::WARNING
+                                       | vk::DebugUtilsMessageSeverityFlagsEXT::INFO,
+                               )
+                               .message_type(vk::DebugUtilsMessageTypeFlagsEXT::all())
+                               .pfn_user_callback(Some(vulkan_debug_callback))
+                               .build());
+
+        let name = CString::new(APP_NAME).unwrap();
         let appinfo = vk::ApplicationInfo::builder()
             .application_name(&name)
             .application_version(vk::make_version(0, 1, 0))
-            .api_version(vk::make_version(1, 0, 0));
+            .api_version(vk::make_version(1, 2, 0));
 
         // get required extensions from sdl2
-        let required_extensions = window.vulkan_instance_extensions().unwrap();
+        let required_extensions = self.window.as_ref().unwrap().vulkan_instance_extensions().unwrap();
 
         // dbg!(&required_extensions);
         // dbg!(entry.enumerate_instance_extension_properties().unwrap());
@@ -79,56 +120,46 @@ impl VulkanGod {
         let layer_names = vec![CString::new("VK_LAYER_KHRONOS_validation").unwrap()];
         
         let layers_names_raw: Vec<*const i8> = layer_names
-                .iter()
-                .map(|raw_name| raw_name.as_ptr())
-                .collect();
-            
+            .iter()
+            .map(|raw_name| raw_name.as_ptr())
+            .collect();
+
+        let debug_info_ref = &mut self.debug_info.unwrap();
+        
         let create_info = vk::InstanceCreateInfo::builder()
             .application_info(&appinfo)
             .enabled_layer_names(&layers_names_raw)
             .enabled_extension_names(&required_extensions_raw_const)
-            .push_next(&mut debug_info);
+            .push_next(debug_info_ref);
         
-        let instance: Instance;
-                       
         unsafe {
-            instance = entry
-                .create_instance(&create_info, None)
-                .expect("Instance creation error");
+            self.instance = Some(self.entry.as_ref().unwrap()
+                                 .create_instance(&create_info, None)
+                                 .expect("Instance creation error"));
 
             // recreate them from raw pointers so rust can deallocate them
-            for raw in required_extensions_raw {
-                CString::from_raw(raw);
+                for raw in required_extensions_raw {
+                    CString::from_raw(raw);
+                }
             }
+
+            self
         }
-                       
-        return instance
-    }
+        
+    fn setup_debug_messenger(&mut self) -> &mut Self {        
+        self.debug_utils_loader = Some(
+            DebugUtils::new(self.entry.as_ref().unwrap(),
+                            self.instance.as_ref().unwrap())
+        );
 
-    fn create_debug_info() -> vk::DebugUtilsMessengerCreateInfoEXT {
-        vk::DebugUtilsMessengerCreateInfoEXT::builder()
-            .message_severity(
-                vk::DebugUtilsMessageSeverityFlagsEXT::ERROR
-                    | vk::DebugUtilsMessageSeverityFlagsEXT::WARNING
-                    | vk::DebugUtilsMessageSeverityFlagsEXT::INFO,
-            )
-            .message_type(vk::DebugUtilsMessageTypeFlagsEXT::all())
-            .pfn_user_callback(Some(vulkan_debug_callback))
-            .build()
-    }
-
-    fn create_debug_utils_loader(entry: &Entry, instance: &Instance, debug_info: &vk::DebugUtilsMessengerCreateInfoEXT) -> (DebugUtils, ash::vk::DebugUtilsMessengerEXT) {
-        let debug_utils_loader = DebugUtils::new(entry, instance);
-
-        let debug_call_back: vk::DebugUtilsMessengerEXT;
         unsafe {
-            debug_call_back = debug_utils_loader
-                .create_debug_utils_messenger(debug_info, None)
-                .unwrap();
+            self.debug_call_back = Some(self.debug_utils_loader.as_ref().unwrap()
+                                        .create_debug_utils_messenger(&self.debug_info.unwrap(), None)
+                                        .unwrap());
         }
 
-        (debug_utils_loader, debug_call_back)
-    }
+        self
+                   }
 }
 
 unsafe extern "system" fn vulkan_debug_callback(
@@ -175,5 +206,5 @@ impl Drop for VulkanGod {
 }
 
 pub fn main() {    
-    let _vg = VulkanGod::new(800, 600);
+    let _vg = VulkanGodBuilder::new(800, 600).build();
 }
